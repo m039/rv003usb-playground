cdcb1e60fa653a5467854851dfe23dda6b85af11
Dmitry Mozgin <m0391n@gmail.com>
2025-08-28 03:05:40 +0300

    Update

---
+++ Libraries/Common/include/prototype_boards.h
@@ -0,0 +1,26 @@
+#ifndef _USB_PROTOTYPE_BOARDS_H
+#define _USB_PROTOTYPE_BOARDS_H
+
+#if defined(PROTOTYPE_BOARD_V1A)
+
+#define LED PD6
+#define BUTTON PA2
+
+#elif defined(PROTOTYPE_BOARD_V1B)
+
+#define LED PD6
+#define BUTTON PA2
+
+#elif defined(PROTOTYPE_BOARD_V1C)
+
+#define LED PD6
+#define BUTTON PA2
+
+#elif defined(PROTOTYPE_BOARD_V2A)
+
+#define LED PA1
+#define BUTTON PA2
+
+#endif
+
+#endif 
